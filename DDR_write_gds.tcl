a74358afcce4b193b7e2a12067a2b7fb5d3e475c
sandeepp-ghub <69347771+sandeepp-ghub@users.noreply.github.com>
2023-11-28 16:34:41 +0530

    Create DDR_write_gds.tcl

---
+++ DDR_write_gds.tcl
@@ -0,0 +1,8 @@
+write_stream ./dwc_ddrphy_pnr_block.gds -format stream -map_file /proj/ccpd01/pkgs/tdb_bundle_ccpd5n_1p0/V007/tech/enc.strmoutmap -unit 2000 -die_area_as_boundary \
+-merge {/proj/tools01/wa/lioral/MISL_DESGIN/dwc_ddr54_tsmc5ffp1_0d99a_mvl/0.99a_mvl/acx4_ew/0.99a/gds/16M_1X_h_1Xb_v_1Xe_h_1Ya_v_1Yb_h_4Y_vhvh_2Yy2Yx2R/dwc_ddrphyacx4_top_ew.gds \
+/proj/tools01/wa/lioral/MISL_DESGIN/dwc_ddr54_tsmc5ffp1_0d99a_mvl/0.99a_mvl/dbyte_ew/0.99a/gds/16M_1X_h_1Xb_v_1Xe_h_1Ya_v_1Yb_h_4Y_vhvh_2Yy2Yx2R/dwc_ddrphydbyte_top_ew.gds \
+/proj/tools01/wa/lioral/MISL_DESGIN/dwc_ddr54_tsmc5ffp1_0d99a_mvl/0.99a_mvl/master/0.99a/gds/16M_1X_h_1Xb_v_1Xe_h_1Ya_v_1Yb_h_4Y_vhvh_2Yy2Yx2R/dwc_ddrphymaster_top.gds \
+/proj/tools01/wa/lioral/MISL_DESGIN/dwc_ddr54_tsmc5ffp1_0d99a_mvl/0.99a_mvl/utility_blocks/1.30a/gds/16M_1X_h_1Xb_v_1Xe_h_1Ya_v_1Yb_h_4Y_vhvh_2Yy2Yx2R/dwc_ddrphy_utility_blocks.gds \
+/proj/tools01/wa/lioral/MISL_DESGIN/bumps/TSMC_BOP/UBM_80/rev1.0/gds/TSMC_BOP_BUMPS.gds \
+}
+
