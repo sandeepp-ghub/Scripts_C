32ca32c6fb5a96cc9eb27adc203a4e40a74b1e49
sandeepp-ghub <69347771+sandeepp-ghub@users.noreply.github.com>
2023-11-29 17:40:04 +0530

    Create ICC_my_print_list.tcl

---
+++ ICC_my_print_list.tcl
@@ -0,0 +1,37 @@
+#====================================================================#
+#                       M A R V E L L  SOC                           #
+#                       ==============                               #
+# Date: 22/4/13                                                      #
+#====================================================================#
+# Script Owner: Lior Allerhand                                       #
+#====================================================================#
+# Description: print a list line by line have an regexp flag         #
+#====================================================================#
+# Example: pl $list                                                  #
+#====================================================================#
+
+
+proc pl { args } {
+    parse_proc_arguments -args ${args} results
+    set inlist $results(inlist)
+    if {[info exists results(-regexp)]} { set exp $results(-regexp) }
+
+    if {[info exists results(-regexp)]} {
+        foreach l $inlist {
+            if {[regexp $exp $l]} {
+                puts $l 
+            }
+        }
+    } else {
+        foreach l $inlist {
+            puts $l
+        }
+    }
+    
+}
+
+define_proc_attributes pl \
+    -info "this stuff will print whan user write <porc_name> -help" \
+    -define_args {
+        { inlist "input list"                          "" string    optional}
+        {-regexp "print only list object who regexp"   "" string    optional}}
