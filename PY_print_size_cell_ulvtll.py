67481d1213865899135afcae2f7b1b3a926dc682
sandeepp-ghub <69347771+sandeepp-ghub@users.noreply.github.com>
2023-11-29 15:28:11 +0530

    Create PY_print_size_cell_ulvtll.py

---
+++ PY_print_size_cell_ulvtll.py
@@ -0,0 +1,52 @@
+# Libraries
+import numpy  as np
+import argparse
+import sys
+import glob
+import re
+import gzip
+import globs
+import os
+from glob import glob
+import pprint
+import shlex
+import csv
+
+parser = argparse.ArgumentParser(description='Print size cells command from a timing report no split.',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('Fin' ,default = False , nargs='+',  help='A path to the user run or sta run main work area.')
+#-- parse arguments                        --#
+args = parser.parse_args()
+Fin = args.Fin
+#print(Fin[0])
+
+SCDB =	{}
+
+fin = open(Fin[0], "r")
+lines = fin.readlines()
+for lline in lines:
+    line = str.rstrip(lline)
+    if not re.search(r"BWP", line):
+        continue
+    line_list = line.split()
+    line_list[0] = re.sub("/\w+$",'', line_list[0])
+    #print(line_list[0])
+    line_list[1] = re.sub("\)",'', line_list[1])
+    line_list[1] = re.sub("\(",'', line_list[1])
+    #print(line_list[1])
+    SCDB[line_list[0]] =  line_list[1]
+fin.close()
+
+for cell in  SCDB:
+    if re.search(r"ELVT$", SCDB[cell]):
+        continue
+    if re.search(r"ULVT$", SCDB[cell]):
+        continue
+    if re.search(r"ULVTLL$", SCDB[cell]):
+        continue
+
+    new_cell  = re.sub('DLVT$','DULVTLL', SCDB[cell])
+    if new_cell is  SCDB[cell]:
+        new_cell  = re.sub('DLVTLL$','DULVTLL', SCDB[cell])
+        #new_cell  = re.sub('DLVTLL$','DELVT', SCDB[cell])
+
+    print("size_cell "+cell+" "+new_cell)
