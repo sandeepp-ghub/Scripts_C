4fea11f28dda1f49b25f0436db348025e91cfb57
sandeepp-ghub <69347771+sandeepp-ghub@users.noreply.github.com>
2023-12-05 11:10:08 +0530

    Create PT_assign.tcl

---
+++ PT_assign.tcl
@@ -0,0 +1,100 @@
+
+#SHOULD GET THIS FROM RAW UPF-613 MESSAGING:
+if {0} {
+ to_check {
+ CLK25_OUT CS_B DIS_VAUX_B DL_ACTIVE_LED_B EN_VAUX_B EN_VPRG EXT_MDC EXT_MDC_1P2V EXT_MDIO EXT_MDIO_1P2V EXT_MDIO_PW EXT_POR_B EXT_POR_VMAIN_B FUNC_HIDE GF0_HSSATEST
+ GF0_RXDIN GF0_RXDIP GF0_TXAON GF0_TXAOP GF1_HSSATEST GF1_RXDIN GF1_RXDIP GF1_TXAON GF1_TXAOP GF2_HSSATEST GF2_RXDIN GF2_RXDIP GF2_TXAON GF2_TXAOP
+ GF3_HSSATEST GF3_RXDIN GF3_RXDIP GF3_TXAON GF3_TXAOP GPIO_00 GPIO_01 GPIO_02 GPIO_03 GPIO_04 GPIO_05 GPIO_06 GPIO_07 GPIO_08
+ GPIO_09 GPIO_10 GPIO_11 GPIO_12 GPIO_13 GPIO_14 GPIO_15 GPIO_16 GPIO_17 GPIO_18 GPIO_19 GPIO_20 GPIO_21 GPIO_22
+ GPIO_23 GPIO_24 GPIO_25 GPIO_26 GPIO_27 GPIO_28 GPIO_29 GPIO_30 GPIO_31 HARD_RST_OUT_B I2C_SCL I2C_SDA_0 I2C_SDA_1 I2C_SDA_2
+ I2C_SDA_3 IJUL_CKN IJUL_CKP INT_POR_BYPASS IOVDD_1P2V MODE_0 MODE_1 MODE_2 MODE_3 MODE_4 MODE_5 NCSI_ARBIN NCSI_ARBOUT NCSI_PKGID0
+ NCSI_PKGID1 NCSI_REFCLK NCSI_RXDV NCSI_RXD_0 NCSI_RXD_1 NCSI_RXER NCSI_TXD_0 NCSI_TXD_1 NCSI_TXEN P0_PHY_LASI_B P0_SIGDET P0_SPD0_LED_B P0_SPD1_LED_B P0_SPD2_LED_B
+ P0_TRAFFICLED_B P1_PHY_LASI_B P1_SIGDET P1_SPD0_LED_B P1_SPD1_LED_B P1_SPD2_LED_B P1_TRAFFICLED_B P2_PHY_LASI_B P2_SIGDET P2_SPD0_LED_B P2_SPD1_LED_B P2_SPD2_LED_B P2_TRAFFICLED_B P3_PHY_LASI_B
+ P3_SIGDET P3_SPD0_LED_B P3_SPD1_LED_B P3_SPD2_LED_B P3_TRAFFICLED_B PCIE_DIS PCIE_LINKUP_LED_B PERST_B PLAY_DEAD PRIVLVL_0 PRIVLVL_1 PROCESS_BIN_SEL_0 PROCESS_BIN_SEL_1 PROCESS_BIN_SEL_2
+ PROCESS_BIN_SEL_3 SCLK SEL_VAUX_B SI SMB_CLK SMB_DATA SO SWAP_0 SWAP_1 TCK TDI TDO TEST_0 TEST_1
+ TEST_2 TEST_ACK TEST_CLK TEST_IN0 TEST_IN1 TEST_IN2 TEST_IN3 TEST_IN4 TEST_IN5 TEST_IN6 TEST_IN7 TEST_OUT0 TEST_OUT1 TEST_OUT2
+ TEST_OUT3 TEST_OUT4 TEST_OUT5 TEST_OUT6 TEST_OUT7 TEST_REQ THERMAL_D_N THERMAL_D_P TMS TRST_B TSIO_0 TSIO_1 TSIO_2 TSIO_3
+ UART_RXD UART_TXD VAUX_PRSNT V_PRG0 V_PRG1 WAKE_B ZCAL_HSSEXTRESN ZCAL_HSSEXTRESP ap__ccu_gib_data[12] gser_refc_clkin[0] gser_refc_clkin[1] gser_refc_clkin[2] gser_refc_clkip[0] gser_refc_clkip[1]
+ gser_refc_clkip[2] pll_bypass_clkn pll_bypass_clkp qlm0_r0 qlm0_refn qlm0_refp qlm0_rxn[0] qlm0_rxn[1] qlm0_rxn[2] qlm0_rxn[3] qlm0_rxp[0] qlm0_rxp[1] qlm0_rxp[2] qlm0_rxp[3]
+ qlm0_txn[0] qlm0_txn[1] qlm0_txn[2] qlm0_txn[3] qlm0_txp[0] qlm0_txp[1] qlm0_txp[2] qlm0_txp[3] qlm1_r0 qlm1_refn qlm1_refp qlm1_rxn[0] qlm1_rxn[1] qlm1_rxn[2]
+ qlm1_rxn[3] qlm1_rxp[0] qlm1_rxp[1] qlm1_rxp[2] qlm1_rxp[3] qlm1_txn[0] qlm1_txn[1] qlm1_txn[2] qlm1_txn[3] qlm1_txp[0] qlm1_txp[1] qlm1_txp[2] qlm1_txp[3] qlm2_r0
+ qlm2_refn qlm2_refp qlm2_rxn[0] qlm2_rxn[1] qlm2_rxn[2] qlm2_rxn[3] qlm2_rxp[0] qlm2_rxp[1] qlm2_rxp[2] qlm2_rxp[3] qlm2_txn[0] qlm2_txn[1] qlm2_txn[2] qlm2_txn[3]
+ qlm2_txp[0] qlm2_txp[1] qlm2_txp[2] qlm2_txp[3] qlm3_r0 qlm3_refn qlm3_refp qlm3_rxn[0] qlm3_rxn[1] qlm3_rxn[2] qlm3_rxn[3] qlm3_rxp[0] qlm3_rxp[1] qlm3_rxp[2]
+ qlm3_rxp[3] qlm3_txn[0] qlm3_txn[1] qlm3_txn[2] qlm3_txn[3] qlm3_txp[0] qlm3_txp[1] qlm3_txp[2] qlm3_txp[3] slm_R0 slm_refn slm_refp slm_rxn slm_rxp
+ slm_txn slm_txp vdda
+}
+}
+
+array unset seen
+array unset seen_multi
+array unset ports_of
+set is_supply {}
+foreach port $to_check {
+  set pad_pin [get_pins -quiet -leaf -of [get_nets -quiet $port]]
+  if {[sizeof_collection $pad_pin] > 0} {
+    set rsn [get_related_supply_net $pad_pin]
+    if {[llength $rsn] > 1} {
+      set seen_multi($rsn) $port
+      set rsn [lindex $rsn 0]
+    }
+    set cell [get_cells -of $pad_pin]
+    set lcell [get_lib_cells -of $cell]
+    set lib [get_attribute [get_libs -of $lcell] full_name]
+    set seen($rsn) 1
+    lappend ports_of($rsn) $port
+  } elseif {[sizeof_collection [get_supply_nets -quiet $port]] > 0} {
+    puts "#INFO: $port is a supply net."
+    lappend is_supply $port
+  } else {
+    puts "#ERROR: No pad or supply connected to $port"
+    #puts "  ALL NET SEGMENTS:"
+    foreach_in_collection ns [sort_collection [get_nets -seg -quiet -of [get_ports $port]] full_name] {
+      set netname [get_object_name $ns]
+      puts "#  NET: $netname"
+      foreach_in_collection nsp [get_pins -quiet -of $ns] {
+        set pinname [get_object_name $nsp]
+        if {$pinname != $netname} {
+          puts "#    PIN: [get_object_name $nsp]"
+        }
+      }
+    }
+  }
+}
+
+puts "# INFO: Power supply nets seen:"
+foreach el [lsort [array names seen]] {
+  puts "#   $el"
+}
+puts "# INFO: Power supply ports seen (could be empty):"
+foreach el [lsort $is_supply] {
+  puts "#   $el"
+}
+puts "# WARNING: Multiple supply nets seen (picked first):"
+foreach el [lsort [array names seen_multi]] {
+  puts "#   $el (ex: $seen_multi($el))"
+}
+
+puts "if {0} {"
+puts "set_voltage 0.800 -min 0.800 -object_list {vaux vdda VDD}"
+puts "set_voltage 2.970 -min 2.970 -object_list {VDD_IO_LFT VDD_IO_RGT}"
+puts "}"
+
+puts "puts \"INFO: (pt_proj_multivoltage.tcl) START loading e5_1.0 specific set_related_supply_net commands for special IP voltage domains\""
+foreach rsn [lsort [array names seen]] {
+  puts "##### BEGING ASSIGNMENT OF PORTS TO SUPPLY $rsn ..."
+  puts "set the_supply $rsn"
+  puts "set the_ports {"
+  set i 0
+  foreach el $ports_of($rsn) {
+    puts -nonewline " $el"
+    incr i 1
+    if {$i == 10} { puts -nonewline "\n"; set i 0 }
+  }
+  puts "\n}"
+  puts "foreach a_port \$the_ports {"
+  puts "  set cmd \"set_related_supply_net \$the_supply -object_list \$a_port\""
+  puts "  puts \"INFO: Running \$cmd\""
+  puts "  eval \$cmd"
+  puts "}\n"
+}
+puts "puts \"INFO: (pt_proj_multivoltage.tcl) FINISH loading e5_1.0 specific set_related_supply_net commands for special IP voltage domains\""
